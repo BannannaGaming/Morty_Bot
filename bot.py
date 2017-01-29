3c13182db5944757e2a0b2f75825b35c4fdd4ee2
Simon <simjenner3@gmail.com>
2017-01-29 01:57:00 +0000

    Misc functions

--- bot.py
+++ bot.py
@@ -21,37 +21,49 @@ async def on_message(message):
     else:
         user = "{0.author.mention}".format(message)  # Get user mention
 
-    if message.content.startswith("!playlist"):
-        await client.send_message(message.channel, "Getting links...\nCopy each line and send it again to get mee6 to add it:")
-        to_send = ""
-        playlist_link = message.content.split(" ")[1]
-        print("{} asked for !playlist {}".format(user, playlist_link))
+    try:
+        if message.content.startswith("!playlist"):
+            await client.send_message(message.channel, "Getting links...\nCopy each line and send it again to get mee6 to add it:")
+            to_send = ""
+            playlist_link = message.content.split(" ")[1]
+            print("{} asked for !playlist {}".format(user, playlist_link))
 
-        playlist_id = playlist_link.split("list=")[1]
-        req = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={}&fields=items%2Fsnippet%2FresourceId%2FvideoId&key={}".format(playlist_id, api_key)
+            playlist_id = playlist_link.split("list=")[1]
+            req = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={}&fields=items%2Fsnippet%2FresourceId%2FvideoId&key={}".format(playlist_id, api_key)
 
-        info = requests.get(req)
-        ids = json.loads(info.text)
+            info = requests.get(req)
+            ids = json.loads(info.text)
 
-        sleep(5)
-        for snippet in ids["items"]:
-            video_id = snippet["snippet"]["resourceId"]["videoId"]
-            to_send += "!add https://www.youtube.com/watch?v={}\n".format(video_id)
+            sleep(5)
+            for snippet in ids["items"]:
+                video_id = snippet["snippet"]["resourceId"]["videoId"]
+                to_send += "!add https://www.youtube.com/watch?v={}\n".format(video_id)
 
-        await client.send_message(message.channel, to_send)
+            await client.send_message(message.channel, to_send)
 
-    elif message.content.startswith("!coinflip"):
-        print("{} asked for coinflip".format(user))
-        flip = random.randint(1, 2)
-        if flip == 1:
-            await client.send_message(message.channel, "Heads wins!")
-        else:
-            await client.send_message(message.channel, "Tails wins!")
+        elif message.content.startswith("!coinflip"):
+            print("{} asked for coinflip".format(user))
+            flip = random.randint(1, 2)
+            if flip == 1:
+                await client.send_message(message.channel, "Heads wins!")
+            else:
+                await client.send_message(message.channel, "Tails wins!")
 
-    elif message.content.startswith("!roll"):
-        print("{} asked for dice roll".format(user))
-        die = random.randint(1, 6)
-        await client.send_message(message.channel, "{} rolled {}".format(user, die))
+        elif message.content.startswith("!roll"):
+            print("{} asked for dice roll".format(user))
+            die = random.randint(1, 6)
+            await client.send_message(message.channel, "{} rolled {}".format(user, die))
+
+        elif message.content.startswith("!choice"):
+            choices = message.content.split(",")[1:]
+            choice = random.randint(0, len(choices)-1)
+            await client.send_message(message.channel, "I choose {}".format(choices[choice]))
+
+        elif message.content.startswith("!help"):
+            await client.send_message(message.channel, "Commands: !playlist, !coinflip, !roll, !choice")
+            
+    except IndexError:
+        await client.send_message(message.channel, "Something went wrong :(")
 
 @client.event
 async def on_ready():
