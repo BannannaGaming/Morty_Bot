753ffb463b262ef2122f0a1b2d865bb3920543a4
Simon <simjenner3@gmail.com>
2017-01-29 02:44:09 +0000

    Misc functions

--- bot.py
+++ bot.py
@@ -54,7 +54,8 @@ async def on_message(message):
             await client.send_message(message.channel, "{} rolled {}".format(user, die))
 
         elif message.content.startswith("!choice"):
-            choices = message.content.split(",")[1:]
+            choices_str = message.content.split(" ", 1)[1:]
+            choices = choices_str.split(",")
             choice = random.randint(0, len(choices)-1)
             await client.send_message(message.channel, "I choose: {}".format(choices[choice]))
 
