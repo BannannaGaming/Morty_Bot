541920ba772c36233511560065679de6c7a0bc10
Simon <simjenner3@gmail.com>
2017-01-30 16:41:47 +0000

    Updates!

---
+++ README.md
@@ -0,0 +1,61 @@
+# Morty-Bot
+
+Morty-Bot is designed to be paried with [mee6](https://mee6.xyz), as the commands have the same format (start with a `!`) and this bot will not contain commands that mee6 has.
+
+Morty-Bot bot work perfectly fine without mee6 if that is your choice.
+
+---
+
+[Add Morty-Bot to your server](https://discordapp.com/oauth2/authorize?client_id=275050313529032706&scope=bot&permissions=0)
+
+---
+
+## Commands
+
+ * Search
+  - `!wiki` `wikipedia page, such as "Star Wars"`
+    + Search and show a snippet of a given wikipedia page
+
+ * Maths
+  - `!solve` `equation to solve`
+    + Solve an equation such as `(x**2+7)*(x+1)`
+
+ * Misc
+  - `!coinflip`
+    + Heads or tails!
+  - `!roll`
+    + Returns a random number between 1 & 6
+  - `!choice` `Comma,Seperated,List,Of,Choices`
+    + Pick a value from a given list of choices
+  - `!quote`
+    + Get a random Rick and Morty quote
+  - `!info`
+    + Get information about this bot
+  - `!help`
+    + Shows this menu
+
+---
+
+## Planned updates
+
+Planned command updates:
+ * Search
+  - `!define` `word to define`
+    + Gets a dictionary definition of a word
+
+ * Maths
+  - `!expand` `equation to expand`
+  - `!simplify` `equation to simplify`
+
+ * Misc
+  - `!info`
+    + Improve info supplied
+
+Other planned updates:
+  * Simple website to control Morty-Bot for your server  
+
+---
+
+## Notes
+
+Morty-Bot may eventually become seperate from mee6
--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-from imgur_album_link_xtractor import get_links
 from platform import python_version
 from sympy import *
 import wikipedia
@@ -8,9 +7,6 @@ import discord
 import random
 import os
 
-# Add to server
-# https://discordapp.com/oauth2/authorize?client_id=275050313529032706&scope=bot&permissions=0
-
 # https://gist.github.com/hzsweers/8595628
 # Get env variable(s) from Heroku
 discord_token = os.environ["morty_discord_token"]
@@ -18,28 +14,34 @@ api_key = os.environ["yt_key"]
 
 client = discord.Client()
 
-to_send = ""
-ids = ""
-
 # sympy math things
 x, y, a, b, z = symbols("x y a b z")
 
-req_no_token = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=40&playlistId={}&fields=items(snippet(resourceId(playlistId%2CvideoId)))%2CnextPageToken&key={}"
-req_with_token = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=40&pageToken={}&playlistId={}&fields=items(snippet(resourceId(playlistId%2CvideoId)))%2CnextPageToken&key={}"
-
 help_message = """
-***Commands***:
-**!makelist** *Youtube playlist link*
-**!coinflip**
-**!roll**
-**!choice** *Comma,Seperated,List,Of,Choices*
-**!quote**
-**!wiki** *page, such as "Donald Trump"*
-**!imgurlinks** *album ID*
-**!solve** *equation - must use x y a b z as symbols*
-**!kys**
-**!info**
-**!help**"""
+***Commands***
+
+ - **Search**
+  - `!wiki` `wikipedia page, such as "Star Wars"`
+    - Search and show a snippet of a given wikipedia page
+
+ - **Maths**
+  - `!solve` `equation to solve`
+    - Solve an equation such as `(x**2+7)*(x+1)` *(must only use x,y,a,b,z)*
+
+ - **Misc**
+  - `!coinflip`
+    - Heads or tails!
+  - `!roll`
+    - Returns a random number between 1 & 6
+  - `!choice` `Comma,Seperated,List,Of,Choices`
+    - Pick a value from a given list of choices
+  - `!quote`
+    - Get a random Rick and Morty quote
+  - `!info`
+    - Get information about this bot
+  - `!help`
+    - Shows this menu
+"""
 
 # Multi-line code block
 info_text = """
@@ -53,21 +55,6 @@ Platform       : {}
            discord.__version__,
            platform.system())
 
-#  Message passed in so message.channel get be
-async def add_to_playlist(channel, req, first=False):
-    global ids
-    to_send = "```"
-
-    async with aiohttp.get(req) as info:
-        ids = await info.json()
-
-    for snippet in ids["items"]:
-        video_id = snippet["snippet"]["resourceId"]["videoId"]
-        to_send += "!add https://www.youtube.com/watch?v={}\n".format(video_id)
-
-    to_send += "```"  # Code block text so link thumbnails don't appear
-    await client.send_message(channel, to_send)
-
 @client.event
 async def on_message(message):
     if message.author == client.user:  # Don't reply to self
@@ -76,27 +63,7 @@ async def on_message(message):
         user = "{0.author.mention}".format(message)  # Get user mention
 
     try:
-        if message.content.startswith("!makelist "):
-            await client.send_message(message.channel, "Getting links...\nCopy each line and send it again to get mee6 to add it:")
-
-            playlist_link = message.content.split(" ")[1]
-            print("{} asked for !playlist {}".format(user, playlist_link))  # Needed so I can see if a (large) playlist caused it to break
-
-            playlist_id = playlist_link.split("list=")[1]
-            await add_to_playlist(message.channel, req_no_token.format(playlist_id, api_key), True)
-
-            while 1:
-                try:
-                    nextpagetoken = ids["nextPageToken"]
-                    print("Next page", nextpagetoken)
-                    await add_to_playlist(message.channel, req_with_token.format(nextpagetoken, playlist_id, api_key))
-
-                except KeyError:
-                        break  # No next page
-
-            await client.send_message(message.channel, "Finished retrieving playlist")
-
-        elif message.content.startswith("!coinflip"):
+        if message.content.startswith("!coinflip"):
             flip = random.randint(1, 2)
             if flip == 1:
                 await client.send_message(message.channel, "Heads wins :ok_hand:")
@@ -133,16 +100,6 @@ async def on_message(message):
                 await client.send_message(message.channel, "That does not match any Wikipedia pages")
             except wikipedia.exceptions.DisambiguationError:
                 await client.send_message(message.channel, "Multiple results found, try something else")
-
-        elif message.content.startswith("!imgurlinks "):
-            albumID = message.content.split(" ", 1)[1]
-            links = "\n".join(await get_links(albumID))
-            if links != "Error":
-                for album_links in [links[i:i + 1800] for i in range(0, len(links), 1800)]:  # Split in to 1800 char chunks
-                    await client.send_message(message.channel, album_links)
-                await client.send_message(message.channel, "Finshed getting links")
-            else:
-                await client.send_message(message.channel, "Invalid album ID")
 
         elif message.content.startswith("!solve "):  # Needs improving
             eq = message.content.split(" ", 1)[1]
