62255a0f2f0f984edfdf49c208fc16eca064a65d
Simon <simjenner3@gmail.com>
2017-02-14 21:04:56 +0000

    !big

--- bot.py
+++ bot.py
@@ -85,7 +85,7 @@ Platform       : {}
            platform.system())
 
  # http://www.hubtraffic.com
-ph = "http://www.pornhub.com/webmasters/search?id=44bc40f3bc04f65b7a35&ordering=rating&period=alltime&thumbsize=small&search={}"
+ph = "http://www.pornhub.com/webmasters/search?id=44bc40f3bc04f65b7a35&ordering=mostviewed&period=alltime&thumbsize=small&search={}"
 
 ph_text = """
 `Title    : {}`
@@ -95,6 +95,35 @@ ph_text = """
 `Link     :` {}
 """  # Last 2 are desc and link
 
+# BIGGGG
+big_dict = {
+    "a":":regional_indicator_a:",
+    "b":":regional_indicator_b:",
+    "c":":regional_indicator_c:",
+    "d":":regional_indicator_d:",
+    "e":":regional_indicator_e:",
+    "f":":regional_indicator_f:",
+    "g":":regional_indicator_g:",
+    "h":":regional_indicator_h:",
+    "i":":regional_indicator_i:",
+    "j":":regional_indicator_j:",
+    "k":":regional_indicator_k:",
+    "l":":regional_indicator_l:",
+    "m":":regional_indicator_m:",
+    "n":":regional_indicator_n:",
+    "o":":regional_indicator_o:",
+    "p":":regional_indicator_p:",
+    "q":":regional_indicator_q:",
+    "r":":regional_indicator_r:",
+    "s":":regional_indicator_s:",
+    "t":":regional_indicator_t:",
+    "u":":regional_indicator_u:",
+    "v":":regional_indicator_v:",
+    "w":":regional_indicator_w:",
+    "x":":regional_indicator_x:",
+    "y":":regional_indicator_y:",
+    "z":":regional_indicator_z:"
+}
 
 # I blame Sam
 async def dirty_stuff(search_term):
@@ -154,6 +183,19 @@ async def get_definition(word):
 
     except IndexError:
         return "{} cannot be found".format(word)
+
+
+async def big(words):
+    output = []
+    for word in words.split(" "):
+        for letter in word.lower():
+            try:
+                output.append(big_dict[letter])
+            except KeyError:
+                output.append("?")
+
+        output.append(" ")
+    return output
 
 
 @client.event
@@ -219,6 +261,11 @@ async def on_message(message):
             ud_word = message.content.split(" ", 1)[1]
             ud_to_send = await get_urban_def(ud_word)
             await client.send_message(message.channel, ud_to_send[:2000])
+
+        elif message.content.lower().startswith("!big "):
+            words_to_big = message.content.split(" ", 1)[1]
+            bigger_words = big(words_to_big)
+            await client.send_message(message.channel, " ".join(bigger_words))
 
         elif message.content.lower().startswith("!info"):
             await client.send_message(message.channel, info_text)
